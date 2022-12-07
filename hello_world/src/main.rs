62dcac153fbe88008e4362980653c095e0eb0eac
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>
2022-12-07 15:36:08 +0100

    chore: cargo add env_logger log

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,8 +1,24 @@
 use regex::Regex;
 
+
+fn execute_query(query: &str) {
+    log::debug!("Executing query: {}", query);
+}
+
+
 fn main() {
     println!("Hello, world!");
 
+    env_logger::init();
+
+    execute_query("DROP TABLE students");
+
     let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
     println!("Did our date match? {}", re.is_match("2014-01-01"));
+
+    let re = Regex::new(r"(\d{4})-(\d{2})-(\d{2})").unwrap();
+    let text = "2012-03-14, 2013-01-01 and 2014-07-05";
+    for cap in re.captures_iter(text) {
+        println!("Month: {} Day: {} Year: {}", &cap[2], &cap[3], &cap[1]);
+    }
 }
